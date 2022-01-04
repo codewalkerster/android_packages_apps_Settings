b5c3e0d933b2d5ff70dc4cb6a7eb01a2c4a99469
Chris <codewalker@hardkernel.com>
2022-01-04 11:09:29 +0900

    ODROID-M1/M1S/M2: Remove starting messege by kiosk prop.
    Change-Id: Ib29f19d4e58f5f2b0bcc59d700a428cbf877b173

--- src/com/android/settings/FallbackHome.java
+++ src/com/android/settings/FallbackHome.java
@@ -30,6 +30,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
+import android.os.SystemProperties;
 import android.os.SystemClock;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -46,6 +47,7 @@ public class FallbackHome extends Activity {
 
     private boolean mProvisioned;
     private WallpaperManager mWallManager;
+    private boolean mKIOSK;
 
     private final Runnable mProgressTimeoutRunnable = () -> {
         View v = getLayoutInflater().inflate(
@@ -107,20 +109,25 @@ public class FallbackHome extends Activity {
 
         registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_USER_UNLOCKED));
         maybeFinish();
+        mKIOSK = SystemProperties.getBoolean("persist.kiosk_mode", false);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (mProvisioned) {
-            mHandler.postDelayed(mProgressTimeoutRunnable, mProgressTimeout);
+            if (!mKIOSK) {
+                mHandler.postDelayed(mProgressTimeoutRunnable, mProgressTimeout);
+            }
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mHandler.removeCallbacks(mProgressTimeoutRunnable);
+        if (!mKIOSK) {
+            mHandler.removeCallbacks(mProgressTimeoutRunnable);
+        }
     }
 
     protected void onDestroy() {
