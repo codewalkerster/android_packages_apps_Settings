276801b5ff51976d2b042a5ed60b48be563ebe86
codewalkerster <codewalker@hardkernel.com>
2025-07-11 15:00:54 +0900

    ODROID: Settings: Fix craching for accessibility when b/t off.
    Change-Id: Ib8f3e7d82c7c2cd626bb83803927744d7df8a5bd

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -25,6 +25,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArrayMap;
@@ -188,8 +189,10 @@ public class AccessibilitySettings extends DashboardFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        use(AccessibilityHearingAidPreferenceController.class)
-                .setFragmentManager(getFragmentManager());
+        if ((SystemProperties.get("persist.bt.service.down")).equals("false")) {
+            use(AccessibilityHearingAidPreferenceController.class)
+                    .setFragmentManager(getFragmentManager());
+        }
     }
 
     @Override
